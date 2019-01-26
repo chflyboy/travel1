b5bfe7e8068fba6fca8e0ccc91a71f157445fba6
fly <2271093089@qq.com>
2019-01-26 20:26:59 +0800

    删除

--- travel/src/main/java/cn/itcast/travel/service/CategoryService.java
+++
@@ -1,9 +0,0 @@
-package cn.itcast.travel.service;
-
-public interface CategoryService {
-    /**
-     * 查询所有的导航条目录
-     * @return
-     */
-    String findNav();
-}
--- travel/src/main/java/cn/itcast/travel/service/FavoriteService.java
+++
@@ -1,9 +0,0 @@
-package cn.itcast.travel.service;
-
-public interface FavoriteService {
-
-    Boolean isFavorite(int uid, int rid);
-
-    void saveFavorite(int uid, int rid);
-
-}
